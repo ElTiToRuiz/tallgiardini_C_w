773187e9f34fe8ca8920086500254f11c15a6a31
Igor Ruiz <ruizigor16@gmail.com>
2025-04-10 00:44:54 +0200

    first commit

---
+++ src/components/react/App.tsx
@@ -0,0 +1,19 @@
+import CallToAction from "./CTA"
+import FunnelSection from "./FunnelSelection"
+import Header from "./Header"
+import Hero from "./Hero"
+import ModelSection from "./ModelSelection"
+import Testimonial from "./Testimonial"
+
+export const App = () => {
+	return (
+		<div >
+			<Header />
+			<Hero />
+			{/* <FunnelSection />
+			<Testimonial />
+			<ModelSection />
+			<CallToAction /> */}
+		</div>
+	)
+}
