39a5b4871b6adf56a315a7c43148e040b5c24904
Igor Ruiz <ruizigor16@gmail.com>
2025-06-24 19:34:25 +0200

    feat: Integrate sitemap support and enhance SEO with updated configurations and metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations:[sitemap()],
+  site:"https://www.tallgiardini.site",
 });
